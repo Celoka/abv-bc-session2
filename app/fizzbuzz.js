905dd48720e5387235eceb5602232631994208c3
Celoka <eloka.chima@gmail.com>
2017-05-18 09:54:28 +0100

    A modified fizzbuzz file

--- app/fizzbuzz.js
+++ app/fizzbuzz.js
@@ -1,6 +1,7 @@
 module.exports =  {
+
 fizzBuzz: (n) => {
-    if ( n % 15 === 0) {
+    if ((n % 3 ===0) && (n % 5 === 0)) {
         return ("FizzBuzz");
    }else if (n % 3 === 0) {
         return ("Fizz");
@@ -8,5 +9,5 @@ fizzBuzz: (n) => {
         return ("Buzz");
    }else
         return (n);
-  }
+    }
  }
